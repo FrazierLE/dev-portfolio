110b8b85d6d541a711fd9b2c2f0a9555d919db04
FrazierLE <lauren.e.frazier@gmail.com>
2025-03-30 19:21:43 -0600

    Fix lint error

--- src/components/footer/FooterLayout.tsx
+++ src/components/footer/FooterLayout.tsx
@@ -1,4 +1,3 @@
-import Contact from './Contact';
 import Info from './Info';
 import { Stack } from '@mui/material';
 
@@ -14,7 +13,6 @@ const FooterLayout = () => {
         sx={{ backgroundColor: '#152a38', borderTop: '1px solid #556e53' }}
       >
         <Info />
-        {/* <Contact /> */}
       </Stack>
     </>
   );
